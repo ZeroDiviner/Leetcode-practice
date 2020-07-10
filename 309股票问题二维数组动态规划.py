35ff8b2621b92a2eefce57b519f1ba24f0350170
Jasper <zhoujasper818@gmail.com>
2020-07-11 02:16:53 +1000

    leetcode 309

---
+++ 309股票问题二维数组动态规划.py
@@ -0,0 +1,84 @@
+﻿
+import time
+##class TreeNode:
+##     def __init__(self, x):
+##         self.val = x
+##         self.left = None
+##         self.right = None
+
+##class ListNode:
+##     def __init__(self, x):
+##          self.val = x
+##          self.next = None
+
+##class RandomListNode:
+##     def __init__(self, x):
+##          self.label = x
+##          self.next = None
+##          self.random = None
+from functools import wraps
+def wrapper(func):
+     
+     @wraps(func)
+     def wrapper_function(self,*arc):
+          start=time.time()
+          res = func(self,*arc)
+          end = time.time()
+          print(f"Your function used {(end-start):.2f} seconds")
+          print(f'Return value of your function is : \n{res}')
+          return res
+     return wrapper_function
+
+from collections import defaultdict
+class Solution:
+    @wrapper
+    def maxProfit(self, prices):
+        # 方法1: 二维数组动态规划
+        if not prices: return 0
+        length = len(prices)
+        dp = [[0 for i in range(3)] for i in range(length+1)]
+        # 0 == have stock, dont' have to rest
+        # 1 == don't have stock ,can buy
+        # 2 == dont'have stock, cannot buy
+        dp[0][0] = -prices[0]
+        for i in range(1,length):
+            dp[i][0] = max(dp[i-1][1]-prices[i], dp[i-1][0])
+            ## state0 can be derive from: 
+            ## 1. last state is 1, last state bought stock
+            ## 2. last state is 0, don't do anything
+            dp[i][1] = max(dp[i-1][2], dp[i-1][1])
+            ## state1 can be derive from:
+            ## 1. have to rest in the last day
+            ## 2. don't have to rest in the last day
+            dp[i][2] = dp[i][0]+prices[i]
+            ## state2 can be derive from only: last timestep, sold stock
+        return max(dp[length-1][1],dp[length-1][2])
+
+
+             
+        
+
+
+
+
+
+def main():
+    
+
+    s = Solution()
+    s.maxProfit([1,2,3,0,2])
+
+    
+    
+
+main()
+
+
+
+
+
+
+
+
+
+
---
+++ 309股票问题有限状态机.py
@@ -0,0 +1,83 @@
+﻿
+import time
+##class TreeNode:
+##     def __init__(self, x):
+##         self.val = x
+##         self.left = None
+##         self.right = None
+
+##class ListNode:
+##     def __init__(self, x):
+##          self.val = x
+##          self.next = None
+
+##class RandomListNode:
+##     def __init__(self, x):
+##          self.label = x
+##          self.next = None
+##          self.random = None
+from functools import wraps
+def wrapper(func):
+     
+     @wraps(func)
+     def wrapper_function(self,*arc):
+          start=time.time()
+          res = func(self,*arc)
+          end = time.time()
+          print(f"Your function used {(end-start):.2f} seconds")
+          print(f'Return value of your function is : \n{res}')
+          return res
+     return wrapper_function
+
+from collections import defaultdict
+class Solution:
+    @wrapper
+    def maxProfit(self, prices):
+        ## 方法二： 有限状态机
+        if not prices: return 0
+        ## Finite state maching
+        ## 在某个时间t，一定有 3 种states：
+        ## 1. have状态，上一状态可能是have，或者canbuy
+        ## 2. canbuy状态，上一状态可能是canbuy然后这一时间不变，或者上一时间cant_buy，这一时间是可以买的
+        ## 3. cant_buy状态，上一状态只可能是have，然后这一时间卖掉了
+        length = len(prices)
+        have = -prices[0]
+        cant_buy = -float('Inf')
+        can_buy = 0
+        # cant_buy = 0
+        for i in range(1, length):
+            have_ = have
+            c = can_buy
+            have = max(c - prices[i], have_)
+            can_buy = max(c, cant_buy)
+            cant_buy = have_+prices[i]
+        return max(can_buy, cant_buy)
+
+
+             
+        
+
+
+
+
+
+def main():
+    
+
+    s = Solution()
+    s.maxProfit([1,2,3,0,2])
+
+    
+    
+
+main()
+
+
+
+
+
+
+
+
+
+
